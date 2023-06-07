8442dfdf738c0078a42056283f8807a6b225b8db
YujenCHE <131453901+YujenCHE@users.noreply.github.com>
2023-06-07 16:37:29 +0800

    Update a291u.cpp

--- a291u.cpp
+++ a291u.cpp
@@ -2,28 +2,49 @@
 
 using namespace std;
 
-void nAnB(){
-    int n,i,a,b;
-    char pw[4];
-    char tried[4];
-    
-    cout<<"Please enter the password : \n";
-    cin>>pw;
-    cout<<"try times : ";
-    cin>>n;
-    
-    for(i=1;i<=n;i++){
-        cin>>tried;
-        for(i=0;i<4;i++){
-            if(pw[i]==tried[i]){
-                a++;
-            }
+void calculateNandB(const string& pw, const string& tried, int& a, int& b) {
+    a = 0;
+    b = 0;
+
+    int pwCount[10] = {0};
+    int triedCount[10] = {0};
+
+    for (int i = 0; i < 4; i++) {
+        if (pw[i] == tried[i]) {
+            a++;
+        } else {
+            pwCount[pw[i] - '0']++;
+            triedCount[tried[i] - '0']++;
         }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        b += min(pwCount[i], triedCount[i]);
     }
 }
 
-int main()
-{
+void nAnB() {
+    int n;
+    string pw;
+    
+    cout << "Please enter the password: ";
+    cin >> pw;
+    cout << "Number of attempts: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string tried;
+        cout << "Attempt " << i + 1 << ": ";
+        cin >> tried;
+
+        int a, b;
+        calculateNandB(pw, tried, a, b);
+
+        cout << a << "A" << b << "B" << endl;
+    }
+}
+
+int main() {
     nAnB();
     return 0;
 }
